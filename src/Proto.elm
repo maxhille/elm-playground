2a9f3d72f797332f48f713df10d083ff86898d81
Maximilian Hille <mh@lambdasoup.com>
2019-05-06 13:58:49 +0200

    (too) simple varint parser

--- src/Proto.elm
+++ src/Proto.elm
@@ -1,5 +1,6 @@
 module Proto exposing (parseVarint)
 
+import Bitwise
 import Bytes exposing (Bytes, Endianness(..))
 import Bytes.Decode exposing (..)
 import Maybe
@@ -8,5 +9,35 @@ import Maybe
 parseVarint : Bytes -> Int
 parseVarint bytes =
     bytes
-        |> decode unsignedInt8
+        |> decode varint
         |> Maybe.withDefault 0
+
+
+varint : Decoder Int
+varint =
+    unsignedInt8
+        |> andThen
+            (\x ->
+                if isFollowed x then
+                    map (\y -> x - 0x80 + Bitwise.shiftLeftBy 7 y) unsignedInt8
+
+                else
+                    succeed x
+            )
+
+
+type alias VarintState =
+    { isFollowed : Bool
+    , sum : Int
+    }
+
+
+
+-- varintStep : VarintState -> Decoder (Step VarintState) Int
+-- varintStep state =
+--       if state.isFollowed then
+
+
+isFollowed : Int -> Bool
+isFollowed n =
+    Bitwise.and n 0x80 == 0x80
